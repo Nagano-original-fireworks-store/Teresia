31df3eeeebda7215ff3a66f6de82aae870676ac2
空 <92256031+wcjqwq@users.noreply.github.com>
2024-11-02 00:11:41 +0800

    Update Dispatch 应该好了？

--- Dispatch/Dispatch.cs
+++ Dispatch/Dispatch.cs
@@ -16,21 +16,17 @@ using System.Reflection;
 using SharedLibrary;
 using System.Text;
 using Configs = SharedLibrary.Config.Config;
+using Newtonsoft.Json.Linq;
 namespace Dispatch
 {
     public class Dispatch : IRouteProvider
     {
         public static Dictionary<string, Proto.DispatchAllInOne.RegionInfo> RegionInfos = new Dictionary<string, Proto.DispatchAllInOne.RegionInfo>();
         public static List<RegionSimpleInfo> regionSimpleInfos = [];
-        public readonly string ListClientStr = @"""{
-    ""sdkenv"": ""2"",
-    ""checkdevice"": false,
-    ""loadPatch"": false,
-    ""showexception"": false,
-    ""regionConfig"": ""pm|fk|add"",
-    ""downloadMode"": ""0""
-}""";
-        public readonly string CurrClientStr = @"""{""coverSwitch"": [""8""], ""perf_report_config_url"": ""http://127.0.0.1/api/perf_report_config/config/verify"", ""perf_report_record_url"": ""http://127.0.0.1/api/perf_report_record/dataUpload""}""";
+        //TODO ListClientStr和CurrClientStr的json正确格式化
+        //public readonly string ListClientStr = "{\"sdkenv\":\"2\",\"checkdevice\":false,\"loadPatch\":false,\"showexception\":false,\"regionConfig\":\"pm|fk|add\",\"downloadMode\":\"0\"}";
+
+        public static Dictionary<string,object> ListClientStr = new Dictionary<string, object>();
 
 #pragma warning disable 8601, 8602 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑添加 "required" 修饰符或声明为可为 null。
         public static readonly List<DispatchConfig> dataList = JsonConvert.DeserializeObject<List<DispatchConfig>>(File.ReadAllText(Configs.ProgramConfig.Current.DataFolder + MethodBase.GetCurrentMethod().DeclaringType.Namespace + "/region.json"));
@@ -65,18 +61,47 @@ namespace Dispatch
 
         private Task<Response> Dispatchs(HttpContext context)
         {
-            RegionCustomConfigE.RegionCustomConfig config = new RegionCustomConfigE.RegionCustomConfig();
+            
+            
             QueryRegionListHttpRsp regionListHttpRsp = new QueryRegionListHttpRsp();
             regionListHttpRsp.Retcode = 0;
             regionListHttpRsp.RegionList.Add(regionSimpleInfos);
             regionListHttpRsp.ClientSecretKey = ByteString.FromStream(new FileStream(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bSeed.bin", FileMode.Open, FileAccess.Read));
-            regionListHttpRsp.ClientCustomConfigEncrypted = ByteString.FromStream(
-                new MemoryStream(
-                    Xor.XorEncryptDecrypt(
-                        Encoding.UTF8.GetBytes(ListClientStr), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+
+            if (!string.IsNullOrEmpty(context.Request.Query["version"]))
+            {
+                if (context.Request.Query["version"].ToString().Substring(0, 2) == "CN")
+                {
+                    regionListHttpRsp.ClientCustomConfigEncrypted = ByteString.FromStream(
+                    new MemoryStream(
+                        Xor.XorEncryptDecrypt(
+                            Encoding.UTF8.GetBytes(ListClientStr[context.Request.Query["version"].ToString().Substring(0, 2)].ToString().ToCharArray()), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+                            )
                         )
-                    )
-                );
+                    );
+                }
+                else if(context.Request.Query["version"].ToString().Substring(0, 2) == "OS")
+                {
+                    regionListHttpRsp.ClientCustomConfigEncrypted = ByteString.FromStream(
+                    new MemoryStream(
+                        Xor.XorEncryptDecrypt(
+                            Encoding.UTF8.GetBytes(ListClientStr[context.Request.Query["version"].ToString().Substring(0, 2)].ToString().ToCharArray()), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+                            )
+                        )
+                    );
+                }else
+                {
+                    regionListHttpRsp.ClientCustomConfigEncrypted = ByteString.FromStream(
+                    new MemoryStream(
+                        Xor.XorEncryptDecrypt(
+                            Encoding.UTF8.GetBytes(ListClientStr["DEF"].ToString().ToCharArray()), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+                            )
+                        )
+                    );
+                }
+            }
+
+            
             regionListHttpRsp.EnableLoginPc = true;
             return Task.FromResult(Http.NewResponse(200, null, Convert.ToBase64String(regionListHttpRsp.ToByteArray())));
         }
@@ -134,24 +159,24 @@ namespace Dispatch
 #pragma warning restore 8602
             NewQueryCurrRegionHttpRsp newQueryCurrRegionHttpRsp = new NewQueryCurrRegionHttpRsp();
             newQueryCurrRegionHttpRsp.RegionInfo = regionInfo;
-            newQueryCurrRegionHttpRsp.ClientRegionCustomConfigEncrypted = ByteString.FromStream(
-                new MemoryStream(
-                    Xor.XorEncryptDecrypt(
-                        Encoding.UTF8.GetBytes(CurrClientStr), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
-                        )
-                    )
-                );
-            //newQueryCurrRegionHttpRsp.RegionCustomConfigEncrypted = ByteString.FromStream(
+            //newQueryCurrRegionHttpRsp.ClientRegionCustomConfigEncrypted = ByteString.FromStream(
             //    new MemoryStream(
             //        Xor.XorEncryptDecrypt(
             //            Encoding.UTF8.GetBytes(CurrClientStr), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
             //            )
             //        )
             //    );
+            newQueryCurrRegionHttpRsp.RegionCustomConfigEncrypted = ByteString.FromStream(
+                new MemoryStream(
+                    Xor.XorEncryptDecrypt(
+                        Encoding.UTF8.GetBytes(dataList.FirstOrDefault(d => d.Name == context.Request.HttpContext.GetRouteValue("region").ToString()).Ext.RegionCustomConfig.ToString(Formatting.None).ToArray()), File.ReadAllBytes(Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+                        )
+                    )
+                );
             newQueryCurrRegionHttpRsp.ClientSecretKey = ByteString.FromStream(
                 new MemoryStream(
                     File.ReadAllBytes(
-                        Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bKey.bin")
+                        Configs.ProgramConfig.Current.DataFolder + "/keys/Ec2bSeed.bin")
                     )
                 );
             byte[] encdata = Rsa.BlockEncrypt(newQueryCurrRegionHttpRsp.ToByteArray(), Rsa.GetPublicKeyFromPem(Configs.ProgramConfig.Current.DataFolder + "/keys/gen/public_key.pem"));
@@ -164,17 +189,40 @@ namespace Dispatch
             };
             return Task.FromResult(Http.NewResponse(200, "application/json;", JsonConvert.SerializeObject(regionRsp)));
         }
-        private static Proto.DispatchAllInOne.RegionInfo RegionInfo()
-        {
-            Proto.DispatchAllInOne.RegionInfo regionInfo = new Proto.DispatchAllInOne.RegionInfo();
-
-
-
-
-            return regionInfo;
-        }
         private static void GenDispatchListAndCurr()
         {
+            string ListConfigJsonContent = File.ReadAllText(Configs.ProgramConfig.Current.DataFolder + MethodBase.GetCurrentMethod().DeclaringType.Namespace + "/list.json");
+            if (string.IsNullOrEmpty(ListConfigJsonContent)) {
+                throw new ArgumentNullException("应有至少一个并且名为 “DEF” 客户端配置 !!!");
+            }
+            ListClientJson listConfigJson = JsonConvert.DeserializeObject<ListClientJson>(ListConfigJsonContent);
+
+            if (listConfigJson.Def != null)
+            {
+                ListClientStr.Add("DEF", listConfigJson.Def);
+            }
+            else {
+                throw new ArgumentNullException("DEF 配置为null请重新检查data文件夹下的文件内容");
+            }
+
+            if (listConfigJson.Cn != null)
+            {
+                ListClientStr.Add("CN", listConfigJson.Cn);
+            }
+            else
+            {
+                throw new ArgumentNullException("CN 配置为null请重新检查data文件夹下的文件内容");
+            }
+
+            if (listConfigJson.Os != null)
+            {
+                ListClientStr.Add("OS", listConfigJson.Os);
+            }
+            else
+            {
+                throw new ArgumentNullException("OS 配置为null请重新检查data文件夹下的文件内容");
+            }
+
 
             string jsonContent = File.ReadAllText(Configs.ProgramConfig.Current.DataFolder + MethodBase.GetCurrentMethod().DeclaringType.Namespace + "/region.json");
             if (string.IsNullOrEmpty(jsonContent))
@@ -293,109 +341,122 @@ namespace Dispatch
         {
             [JsonProperty("update")]
             public Dictionary<string, Proto.DispatchAllInOne.RegionInfo> Update { get; set; }
+
+            [JsonProperty("regionCustomConfig")]
+            public JObject RegionCustomConfig { get; set; }
         }
-        public class RegionCustomConfigE
+        //public class RegionCustomConfigE
+        //{
+        //    public partial class RegionCustomConfig
+        //    {
+        //        [JsonProperty("AliWaterMaskAfa")]
+        //        public long AliWaterMaskAfa { get; set; }
+
+        //        [JsonProperty("AliWaterMaskReqURL")]
+        //        public Uri AliWaterMaskReqUrl { get; set; }
+
+        //        [JsonProperty("AliWaterMaskTimeOut")]
+        //        public long AliWaterMaskTimeOut { get; set; }
+
+        //        [JsonProperty("SDKServerLog")]
+        //        public bool SdkServerLog { get; set; }
+
+        //        [JsonProperty("checkEntityPreloadOverTime")]
+        //        public bool CheckEntityPreloadOverTime { get; set; }
+
+        //        [JsonProperty("codeSwitch")]
+        //        public long[] CodeSwitch { get; set; }
+
+        //        [JsonProperty("coverSwitch")]
+        //        public long[] CoverSwitch { get; set; }
+
+        //        [JsonProperty("dumpType")]
+        //        public long DumpType { get; set; }
+
+        //        [JsonProperty("enableMobileMaskResize")]
+        //        public bool EnableMobileMaskResize { get; set; }
+
+        //        [JsonProperty("engineCodeSwitch")]
+        //        public long[] EngineCodeSwitch { get; set; }
+
+        //        [JsonProperty("greyTest")]
+        //        public GreyTest[] GreyTest { get; set; }
+
+        //        [JsonProperty("mtrConfig")]
+        //        public Config MtrConfig { get; set; }
+
+        //        [JsonProperty("perf_report_config_url")]
+        //        public Uri PerfReportConfigUrl { get; set; }
+
+        //        [JsonProperty("perf_report_percent")]
+        //        public long PerfReportPercent { get; set; }
+
+        //        [JsonProperty("perf_report_record_url")]
+        //        public Uri PerfReportRecordUrl { get; set; }
+
+        //        [JsonProperty("perf_report_servertype")]
+        //        public long PerfReportServertype { get; set; }
+
+        //        [JsonProperty("platformCoverSwitch")]
+        //        public PlatformCoverSwitch PlatformCoverSwitch { get; set; }
+
+        //        [JsonProperty("post_client_data_url")]
+        //        public Uri PostClientDataUrl { get; set; }
+
+        //        [JsonProperty("reportNetDelayConfig")]
+        //        public ReportNetDelayConfig ReportNetDelayConfig { get; set; }
+
+        //        [JsonProperty("showexception")]
+        //        public bool Showexception { get; set; }
+
+        //        [JsonProperty("urlCheckConfig")]
+        //        public Config UrlCheckConfig { get; set; }
+
+        //        [JsonProperty("useLoadingBlockChecker")]
+        //        public bool UseLoadingBlockChecker { get; set; }
+
+        //    }
+        //    public partial class GreyTest
+        //    {
+        //        [JsonProperty("codeSwitchs")]
+        //        public long[] CodeSwitchs { get; set; }
+
+        //        [JsonProperty("platforms")]
+        //        public string[] Platforms { get; set; }
+
+        //        [JsonProperty("rate")]
+        //        public long Rate { get; set; }
+        //    }
+
+        //    public partial class Config
+        //    {
+        //        [JsonProperty("isOpen")]
+        //        public bool IsOpen { get; set; }
+        //    }
+
+        //    public partial class PlatformCoverSwitch
+        //    {
+        //        [JsonProperty("32")]
+        //        public long[] The32 { get; set; }
+        //    }
+
+        //    public partial class ReportNetDelayConfig
+        //    {
+        //        [JsonProperty("openGateserver")]
+        //        public bool OpenGateserver { get; set; }
+        //    }
+        //}
+        public class ListClientJson
         {
-            public partial class RegionCustomConfig
-            {
-                [JsonProperty("AliWaterMaskAfa")]
-                public long AliWaterMaskAfa { get; set; }
+            [JsonProperty("DEF")]
+            public object Def { get; set; }
 
-                [JsonProperty("AliWaterMaskReqURL")]
-                public Uri AliWaterMaskReqUrl { get; set; }
+            [JsonProperty("CN")]
+            public object Cn { get; set; }
 
-                [JsonProperty("AliWaterMaskTimeOut")]
-                public long AliWaterMaskTimeOut { get; set; }
-
-                [JsonProperty("SDKServerLog")]
-                public bool SdkServerLog { get; set; }
-
-                [JsonProperty("checkEntityPreloadOverTime")]
-                public bool CheckEntityPreloadOverTime { get; set; }
-
-                [JsonProperty("codeSwitch")]
-                public long[] CodeSwitch { get; set; }
-
-                [JsonProperty("coverSwitch")]
-                public long[] CoverSwitch { get; set; }
-
-                [JsonProperty("dumpType")]
-                public long DumpType { get; set; }
-
-                [JsonProperty("enableMobileMaskResize")]
-                public bool EnableMobileMaskResize { get; set; }
-
-                [JsonProperty("engineCodeSwitch")]
-                public long[] EngineCodeSwitch { get; set; }
-
-                [JsonProperty("greyTest")]
-                public GreyTest[] GreyTest { get; set; }
-
-                [JsonProperty("mtrConfig")]
-                public Config MtrConfig { get; set; }
-
-                [JsonProperty("perf_report_config_url")]
-                public Uri PerfReportConfigUrl { get; set; }
-
-                [JsonProperty("perf_report_percent")]
-                public long PerfReportPercent { get; set; }
-
-                [JsonProperty("perf_report_record_url")]
-                public Uri PerfReportRecordUrl { get; set; }
-
-                [JsonProperty("perf_report_servertype")]
-                public long PerfReportServertype { get; set; }
-
-                [JsonProperty("platformCoverSwitch")]
-                public PlatformCoverSwitch PlatformCoverSwitch { get; set; }
-
-                [JsonProperty("post_client_data_url")]
-                public Uri PostClientDataUrl { get; set; }
-
-                [JsonProperty("reportNetDelayConfig")]
-                public ReportNetDelayConfig ReportNetDelayConfig { get; set; }
-
-                [JsonProperty("showexception")]
-                public bool Showexception { get; set; }
-
-                [JsonProperty("urlCheckConfig")]
-                public Config UrlCheckConfig { get; set; }
-
-                [JsonProperty("useLoadingBlockChecker")]
-                public bool UseLoadingBlockChecker { get; set; }
-
-            }
-            public partial class GreyTest
-            {
-                [JsonProperty("codeSwitchs")]
-                public long[] CodeSwitchs { get; set; }
-
-                [JsonProperty("platforms")]
-                public string[] Platforms { get; set; }
-
-                [JsonProperty("rate")]
-                public long Rate { get; set; }
-            }
-
-            public partial class Config
-            {
-                [JsonProperty("isOpen")]
-                public bool IsOpen { get; set; }
-            }
-
-            public partial class PlatformCoverSwitch
-            {
-                [JsonProperty("32")]
-                public long[] The32 { get; set; }
-            }
-
-            public partial class ReportNetDelayConfig
-            {
-                [JsonProperty("openGateserver")]
-                public bool OpenGateserver { get; set; }
-            }
+            [JsonProperty("OS")]
+            public object Os { get; set; }
         }
-        
 
     }
 }
